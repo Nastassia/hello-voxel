4911fbfb215915a3c44d9d0af3dd5a25c95d7569
Nastassia <nastassiacarmona@gmail.com>
2015-01-05 13:32:36 -0500

    grunt work

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,1 +1,7 @@
-grunt.registerTask('heroku:production', 'npm start');
+
+module.exports = function(grunt) {
+	grunt.loadNpmTasks('grunt-heroku-deploy');
+	// grunt.registerTask('heroku:production', 'npm start');
+
+};
+
